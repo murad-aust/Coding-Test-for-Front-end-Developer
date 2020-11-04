bc4b0e4fc2e873c8111b6e7a8624486e79b6968e
murad-aust <murad.rony15@gmail.com>
2020-11-04 14:56:56 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Home from './Components/Task-1/Home/Home';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import ImagesPreview from './Components/Task-2_3/ImagesPreview';
+import ImageDraggable from './Components/Task-4_5_6/Draggable';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+            <Home></Home>
+        </Route>
+        <Route path="/imagePreview">
+            <ImagesPreview></ImagesPreview>
+        </Route>
+        <Route path="/draggable">
+          <ImageDraggable></ImageDraggable>
+        </Route>
+      </Switch>
+      
+    </Router>
   );
 }
 
